c7463a0e9270296b04c2b3c0c6bf29162cf21b2b
Valentina <valenthinarojasr@gmail.com>
2026-08-10 15:26:46 -0400

    fix(6.2.2-D): scope service repository operations by tenant

--- Server/src/repositories/service.repository.js
+++ Server/src/repositories/service.repository.js
@@ -8,8 +8,11 @@ export const findById = async (id) => {
   return await Service.findById(id).populate("workers", "firstName lastName email phone");
 };
 
-export const findByIdAndBusiness = async (id, businessId) => {
-  return await Service.findOne({ _id: id, business: businessId })
+export const findByIdAndBusiness = async (id, businessId, { onlyActive = false } = {}) => {
+  const query = { _id: id, business: businessId };
+  if (onlyActive) query.isActive = true;
+
+  return await Service.findOne(query)
     .populate("workers", "firstName lastName email phone");
 };
 
@@ -25,6 +28,18 @@ export const update = async (id, data) => {
   return await Service.findByIdAndUpdate(id, data, { new: true, runValidators: true });
 };
 
+export const updateByIdAndBusiness = async (id, businessId, data) => {
+  return await Service.findOneAndUpdate(
+    { _id: id, business: businessId },
+    data,
+    { new: true, runValidators: true },
+  );
+};
+
 export const deleteById = async (id) => {
   return await Service.findByIdAndDelete(id);
+};
+
+export const deleteByIdAndBusiness = async (id, businessId) => {
+  return await Service.findOneAndDelete({ _id: id, business: businessId });
 };
