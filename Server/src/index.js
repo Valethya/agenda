582860b2e7cfe6a120df6531c38f1f6e48bdc0bb
Valentina <valenthinarojasr@gmail.com>
2026-08-10 19:00:09 -0400

    fix(6.2.3): fail closed before serving incompatible runtime

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,19 +1,27 @@
 import dns from "dns";
+import mongoose from "mongoose";
+
 dns.setDefaultResultOrder("ipv4first");
 
 import { app } from "./app.js";
 import { port } from "./config/env.js";
 import { connectDB } from "./db/db.js";
+import { assertAvailabilityRuntimeStorageReady } from "./db/availability-cutover-gate.js";
 import logger from "./config/logger.js";
 import { initSocket } from "./config/socket.js";
 
-logger.info("server running");
+const startServer = async () => {
+  await connectDB();
+  await assertAvailabilityRuntimeStorageReady(mongoose.connection.db, process.env);
 
-connectDB();
+  const httpServer = app.listen(port, () => {
+    logger.info(`server running at port ${port}`);
+  });
 
-const httpServer = app.listen(port, () => {
-  logger.info(`server running at port ${port}`);
+  initSocket(httpServer);
+};
+
+startServer().catch((error) => {
+  logger.error(`[STARTUP] ${error.message}`);
+  process.exit(1);
 });
-
-// Inicializar el servidor de WebSockets en tiempo real
-initSocket(httpServer);
